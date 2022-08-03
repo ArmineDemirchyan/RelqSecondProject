06a033fbb740cf94611f93f1e5360f736e92ae9c
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-08-03 15:13:44 +0400

    fixed js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "baaf2fd297f31f2f825e9e7891d91a13",
+    apiKey: "baaf2fd297f31f2f825e9e7891d91a13",
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
@@ -15,14 +15,13 @@ let weather = {
         const {icon, description } = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = " https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
         document.querySelector(".description").innerText =description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity  + " %";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
-        document.querySelector("body").style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
-        // document.querySelector(".weather").classList.remove(".loading");
+        document.querySelector(".temp").innerText = `${Math.round(temp-273.15)} °C`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity} %`;
+        document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
+        document.querySelector("body").style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${name}")`;
     },
     searchCity: function(){
         this.fetchWeather(document.getElementsByClassName("search-bar")[0].value);
@@ -41,17 +40,3 @@ let weather = {
         
     })
 
-// let weather = {
-//     apiKey: "b91558a8d3ab4728a0f114301222807",
-
-//     fetchWeather: function (city) {
-//         fetch("https://api.weatherapi.com/v1/current.json?key="
-//             + this.apiKey +
-//             "&q="
-//             + city +
-//             "&aqi=no"
-//         )
-//         .then((response) => response.json())
-//         .then((data) => this.displayWeather(data)); 
-//     }
-// }
